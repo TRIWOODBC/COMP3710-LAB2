d960972d6773c811d3b10e793d76c9a6bbcb898f
林柏成 <lin@Mac-3.modem>
2025-09-04 15:01:04 +1000

    add update.py to update git

---
+++ update.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+def run_cmd(cmd):
+    print(f"→ {cmd}")
+    os.system(cmd)
+
+# 默认 commit 信息
+msg = "add update.py to update git"
+
+# 如果运行时给了参数，就用参数作为 commit 信息
+if len(sys.argv) > 1:
+    msg = " ".join(sys.argv[1:])
+
+# Git 常规操作
+run_cmd("git add .")
+run_cmd(f'git commit -m "{msg}"')
+run_cmd("git push origin main")
